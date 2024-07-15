bf27e517b95440a58e0e937431c5848d9b102048
Edmond Dantès <1213970695@qq.com>
2024-07-15 16:20:35 +0800

    Update duplications_gen.go

--- sonarqube/duplications_gen.go
+++ sonarqube/duplications_gen.go
@@ -7,8 +7,18 @@ import (
 	"net/http"
 )
 
+// AUTOMATICALLY GENERATED, DO NOT EDIT BY HAND!
+
 type Duplications service
 
+// Show - Get duplications. Require Browse permission on file's project
+// Since 4.4
+// Changelog:
+//   9.6: The fields 'subProject', 'subProjectName' were removed from the response.
+//   8.8: Deprecated parameter 'uuid' was removed.
+//   8.8: The fields 'uuid', 'projectUuid', 'subProjectUuid' were removed from the response.
+//   6.5: Parameter 'uuid' is now deprecated.
+//   6.5: The fields 'uuid', 'projectUuid', 'subProjectUuid' are now deprecated in the response.
 func (s *Duplications) Show(ctx context.Context, r duplications.ShowRequest) (*duplications.ShowResponse, *http.Response, error) {
 	u := fmt.Sprintf("%s/show", s.path)
 	v := new(duplications.ShowResponse)
