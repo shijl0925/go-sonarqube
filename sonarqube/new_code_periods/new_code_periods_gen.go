8bb24079f9c8fd937948d143f22546c85b12d29d
shijl0925 <shijl0925@users.noreply.github.com>
2024-09-28 02:44:02 +0000

    Add untracked file during workflow

--- sonarqube/new_code_periods/new_code_periods_gen.go
+++ sonarqube/new_code_periods/new_code_periods_gen.go
@@ -2,7 +2,7 @@ package new_code_periods
 
 // AUTOMATICALLY GENERATED, DO NOT EDIT BY HAND!
 
-// ListRequest Lists the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.7/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> for all branches in a project.<br>Requires the permission to browse the project
+// ListRequest Lists the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.8/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> for all branches in a project.<br>Requires the permission to browse the project
 type ListRequest struct {
 	Project string `url:"project"` // Project key
 }
@@ -19,7 +19,7 @@ type ListResponse struct {
 	} `json:"newCodePeriods,omitempty"`
 }
 
-// SetRequest Updates the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.7/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> on different levels:<br><ul><li>Not providing a project key and a branch key will update the default value at global level. Existing projects or branches having a specific new code definition will not be impacted</li><li>Project key must be provided to update the value for a project</li><li>Both project and branch keys must be provided to update the value for a branch</li></ul>Requires one of the following permissions: <ul><li>'Administer System' to change the global setting</li><li>'Administer' rights on the specified project to change the project setting</li></ul>
+// SetRequest Updates the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.8/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> on different levels:<br><ul><li>Not providing a project key and a branch key will update the default value at global level. Existing projects or branches having a specific new code definition will not be impacted</li><li>Project key must be provided to update the value for a project</li><li>Both project and branch keys must be provided to update the value for a branch</li></ul>Requires one of the following permissions: <ul><li>'Administer System' to change the global setting</li><li>'Administer' rights on the specified project to change the project setting</li></ul>
 type SetRequest struct {
 	Branch  string `form:"branch,omitempty"`  // Branch key
 	Project string `form:"project,omitempty"` // Project key
@@ -27,7 +27,7 @@ type SetRequest struct {
 	Value   string `form:"value,omitempty"`   // Value<br/>For each type, a different value is expected:<ul><li>the uuid of an analysis, when type is SPECIFIC_ANALYSIS</li><li>no value, when type is PREVIOUS_VERSION</li><li>a number between 1 and 90, when type is NUMBER_OF_DAYS</li><li>a string, when type is REFERENCE_BRANCH</li></ul>
 }
 
-// ShowRequest Shows the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.7/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a>.<br> If the component requested doesn't exist or if no new code definition is set for it, a value is inherited from the project or from the global setting.Requires one of the following permissions if a component is specified: <ul><li>'Administer' rights on the specified component</li><li>'Execute analysis' rights on the specified component</li></ul>
+// ShowRequest Shows the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.8/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a>.<br> If the component requested doesn't exist or if no new code definition is set for it, a value is inherited from the project or from the global setting.Requires one of the following permissions if a component is specified: <ul><li>'Administer' rights on the specified component</li><li>'Execute analysis' rights on the specified component</li></ul>
 type ShowRequest struct {
 	Branch  string `url:"branch,omitempty"`  // Branch key
 	Project string `url:"project,omitempty"` // Project key
@@ -41,7 +41,7 @@ type ShowResponse struct {
 	Type       string `json:"type,omitempty"`
 }
 
-// UnsetRequest Unsets the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.7/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> for a branch, project or global. It requires the inherited New Code Definition to be compatible with the Clean as You Code methodology, and one of the following permissions: <ul><li>'Administer System' to change the global setting</li><li>'Administer' rights for a specified component</li></ul>
+// UnsetRequest Unsets the <a href="https://sonar-documentations-preview.netlify.app/sonarqube/10.8/project-administration/clean-as-you-code-settings/defining-new-code/" target="_blank" rel="noopener noreferrer">new code definition</a> for a branch, project or global. It requires the inherited New Code Definition to be compatible with the Clean as You Code methodology, and one of the following permissions: <ul><li>'Administer System' to change the global setting</li><li>'Administer' rights for a specified component</li></ul>
 type UnsetRequest struct {
 	Branch  string `form:"branch,omitempty"`  // Branch key
 	Project string `form:"project,omitempty"` // Project key
